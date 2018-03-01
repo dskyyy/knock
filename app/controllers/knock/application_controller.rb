72553ccda1090599eabee87a099da7930fb3e76e
Dmitry Makeev <dmakeev@gmail.com>
2018-03-01 22:44:48 +0300

    Return JSON if user not found

--- app/controllers/knock/application_controller.rb
+++ app/controllers/knock/application_controller.rb
@@ -2,10 +2,10 @@ module Knock
   class ApplicationController < ActionController::Base
     rescue_from Knock.not_found_exception_class_name, with: :not_found
 
-  private
+    private
 
     def not_found
-      head :not_found
+      render json: { error: "User not found" }, status: :not_found
     end
   end
 end
